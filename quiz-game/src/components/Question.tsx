7d0b50c4a522ca852e2d892c0a1130cfa9decbe6
davidhcodes <dhanlyg1@gmail.com>
2024-09-10 14:36:09 +0100

    Updates question number counter for mobile first design

--- quiz-game/src/components/Question.tsx
+++ quiz-game/src/components/Question.tsx
@@ -16,7 +16,7 @@ const Question: React.FC<QuestionProps> = ({ question, onAnswer, setButton, acti
 
   return (
     <div className=' m-4  flex-col md:m-4 md:flex-col'>
-      <div className='bg-red-950  flex fixed items-center right-0 top-0 justify-center text-center  w-48  mr-20  mt-10  h-32 font-bold rounded-full'> <div className=''><h1 className='  text-white text-5xl'>{currentQuestionIndex+1}/{totalQuestionsNumber}</h1></div></div>
+      <div className='bg-red-950  flex fixed items-center right-0 top-0 justify-center text-center  w-40  mr-2  mt-10  h-20 font-bold rounded-full md:bg-red-950  md:flex md:fixed md:items-center md:right-0 md:top-0 md:justify-center md:text-center  md:w-48  md:mr-20  md:mt-10 md:h-32 md:font-bold md:rounded-full'> <h1 className='  text-white text-4xl md:text-white md:text-5xl'>{currentQuestionIndex+1}/{totalQuestionsNumber}</h1></div>
       <h2 className='pl-5 text-4xl pb-10 text-white text-center mb-8 font-semibold md:pl-5 md:text-4xl  md:text-center md:text-white  md:font-semibold'>{question.question}</h2>
       <div className=' items-center justify-center flex  md:flex'> 
       <ul className='m-auto grid grid-cols-2 gap-8 h-fit place-items-center md:m-auto md:grid md:grid-cols-2 md:gap-4 md:place-items-center'>
@@ -26,7 +26,7 @@ const Question: React.FC<QuestionProps> = ({ question, onAnswer, setButton, acti
           
             <div className='bg-red-950 p-1  md:bg-red-950 md:p-1'>
             <button
-                  className={selectedButton === index ? 'bg-black w-48 h-36 md:bg-black md:w-96 md:h-20' : 'bg-red-400 w-48 h-36 text-red-950 md:bg-white md:w-96 md:h-20 md:text-red-950'}
+                  className={selectedButton === index ? 'bg-black w-48 h-36 md:bg-black md:w-96 md:h-20' : 'bg-white w-48 h-36 text-red-950 md:bg-white md:w-96 md:h-20 md:text-red-950'}
                   onClick={() => handleButtonClick(index)}
                   >
               <h1 className= {activeButton === index? 'text-4xl text-center  text-red-950 md:text-2xl md:text-center md:ml-5 md:text-red-950' : 'text-3xl text-center  text-red-950 md:text-2xl md:text-center md:ml-5 md:text-red-950'}>{option}</h1>
